21c91a9a4af1e6d840c9a72214e4863effdd2eab
lexiarise <rit98t@gmail.com>
2023-02-04 14:36:50 +0200

    general update  meet the team and services main

---
+++ Bonita/application.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+  // Activate Carousel
+  $("#myCarousel").carousel("pause");
+
+  // Click on the button to start sliding
+  $("#myBtn").click(function(){
+    $("#myCarousel").carousel("cycle");
+  });
+
+  // Click on the button to stop sliding
+  $("#myBtn2").click(function(){
+    $("#myCarousel").carousel("pause");
+  });
+
+  // Enable Carousel Indicators
+  $(".item1").click(function(){
+    $("#myCarousel").carousel(0);
+  });
+  $(".item2").click(function(){
+    $("#myCarousel").carousel(1);
+  });
+  $(".item3").click(function(){
+    $("#myCarousel").carousel(2);
+  });
+  $(".item4").click(function(){
+    $("#myCarousel").carousel(3);
+  });
+
+  // Enable Carousel Controls
+  $(".left").click(function(){
+    $("#myCarousel").carousel("prev");
+  });
+  $(".right").click(function(){
+    $("#myCarousel").carousel("next");
+  });
+});
